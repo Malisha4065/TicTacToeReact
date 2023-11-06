846068aa364562f40bd4ce3451a966e1387dcd60
Malisha A.P.D <128911037+Malisha4065@users.noreply.github.com>
2023-11-06 22:30:34 +0530

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
   return (
     <>
-      <p>Coming soon</p>
+      <main>
+        <div id="game-container">
+          <ol id="players">
+            <li><span className="player-name">Player 1</span>
+            <span className="player-symbol">X</span></li>
+            <li>
+              <span className="player-name">Player 2</span>
+              <span className="player-symbol">O</span>
+            </li>
+          </ol>
+        </div>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

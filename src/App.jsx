fbc6938eb29c63774380ac5169c675a46abef3da
Malisha A.P.D <128911037+Malisha4065@users.noreply.github.com>
2023-11-08 21:31:14 +0530

    Player component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./App.css";
+import Player from './components/Player.jsx';
 
 function App() {
   return (
@@ -7,14 +8,12 @@ function App() {
       <main>
         <div id="game-container">
           <ol id="players">
-            <li><span className="player-name">Player 1</span>
-            <span className="player-symbol">X</span></li>
-            <li>
-              <span className="player-name">Player 2</span>
-              <span className="player-symbol">O</span>
-            </li>
+            <Player initialName="Player 1" symbol="X" />
+            <Player initialName="Player 2" symbol="O" />
           </ol>
+          GAME BOARD
         </div>
+        LOG
       </main>
     </>
   );
